dfd0f997f543fe56c3d8ec702c996f2db15c59f5
NKW <2314504058@qq.com>
2018-11-30 11:43:47 +0800

    修改baseFragment

--- app/src/main/java/com/nkw/testframework/presenter/MainPresenterImpl.java
+++ app/src/main/java/com/nkw/testframework/presenter/MainPresenterImpl.java
@@ -1,13 +1,21 @@
 package com.nkw.testframework.presenter;
 
+import android.os.Handler;
+
 import com.base.basecodelib.base.BasePresenterImpl;
 import com.nkw.testframework.contract.MainContract;
+
 
 public class MainPresenterImpl extends BasePresenterImpl<MainContract.View> implements MainContract.Presenter {
 
     @Override
     public void init() {
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mView.showMainText("adjfkhsdjkfhdsdjkhfjksddhffkjdfss");
+            }
+        },3000);
     }
 
     @Override
